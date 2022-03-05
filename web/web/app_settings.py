0bb88cc53aaf8cec041c043cd979e4bf9c973101
Valerio Farrotti <valerio.farrotti@hotmail.it>
2022-03-05 16:35:48 +0100

    Added mapbox style by category

--- web/web/app_settings.py
+++ web/web/app_settings.py
@@ -15,6 +15,6 @@ MAPBOX_TOKEN = os.getenv('MAPBOX_TOKEN', None)
 
 MAPSTYLE_BY_CATEGORY = {
 	'food': 'mapbox://styles/vfarrotti/cl0dlp9d1002o14ta47a6sia5',
-	'house': 'mapbox://styles/mapbox/navigation-night-v1', # This is default map
+	'house': 'mapbox://styles/vfarrotti/cl0dxz6yc002714koas5d74m0',
 	'health': 'mapbox://styles/vfarrotti/cl0dmg2w6000314pm77jn94xr'
 }
